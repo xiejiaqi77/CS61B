0e3c1a1d9ecf5293293b25bd64ef2107e7307b5e
Jiaq <xiejiaqi77.job@gmail.com>
2020-11-01 15:18:15 +0900

    create Body.java for Proj0

---
+++ proj0/Body.java
@@ -0,0 +1,12 @@
+Public class Body {
+        double xxPos;
+        double yyPos;
+        double xxVel;
+        double yyVel;
+        double mess;
+        String imgFileName;
+
+        public Body(double xP, double yP, double xV, double yV, double m, String img)
+        public Body(Body b)
+        
+}
